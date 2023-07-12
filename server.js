ae21e447cab3e369c2e6d34e03a9fa8f1bab2d09
Shashi Shekhar Singh <92780989+Shekhar7u@users.noreply.github.com>
2023-07-12 20:04:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express= require('express');
+const serverConfig=require(`./config/server.config`);
+
+
+const app = express();
+
+
+
+
+app.listen(serverConfig.PORT,()=>{
+  console.log(`server started on the port number ${serverConfig.PORT}`);  
+})
